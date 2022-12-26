c83752dc537aced1c31097217887535b1f2badad
dagmawig <dgebreselasse@gmail.com>
2022-12-25 22:04:03 -0600

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="header container bg-success">
+            <div className='header_row text-center row'>
+                <div className='header_title col'>
+                    WORKOUT
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
