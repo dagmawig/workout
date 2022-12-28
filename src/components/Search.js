6e5d26ebab1dd8b89403cf6f1da1bda7b5cfa794
dagmawig <dgebreselasse@gmail.com>
2022-12-27 19:20:46 -0600

    added options select element

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Search.css';
 var muscles = require('../exerMuscle.json');
-var exercises = require('../exercises.json')
-var bodyParts = require('../exerBody.json')
-
+var exerciseNames = require('../exerciseNames.json');
+var bodyParts = require('../exerBody.json');
 
 function Search() {
-    function handleSelect(e) {
-        console.log("e.value")
+    const [filter, updateFilter] = useState(null);
+    const [disable, updateDisable] = useState(true);
+    function optionsList(list) {
+        return list.map((item, i) => {
+            return (
+                <option value={item} className='small' key ={i+'option'}>{item}</option>
+            )
+        })
     }
+
+    function handleChange(value) {
+        updateFilter(value);
+        if(value==='empty') updateDisable(true);
+        else updateDisable(false);
+    }
+
     return (
         <div className="search container">
             <div className='row search_select'>
@@ -16,18 +28,21 @@ function Search() {
                     Search By :
                 </div>
                 <div className='col-8'>
-                   <select className='form-select' aria-label='Default select'>
-                    <option defaultValue={true} className='small'>Select</option>
-                    <option value='1' className='small'>Body Part</option>
-                    <option value='2' className='small'>Muscle Group</option>
-                    <option value='3' className='small'>Workout Name</option>
+                   <select className='form-select' aria-label='Default select' id='filter' onChange={e=>handleChange(e.target.value)}>
+                    <option defaultValue={true} className='small' value='empty'>Select</option>
+                    <option value='Body Part' className='small'>Body Part</option>
+                    <option value='Muscle Group' className='small'>Muscle Group</option>
+                    <option value='Workout Name' className='small'>Workout Name</option>
                    </select>
                 </div>
                 <div className='col-4'>
                     Options :
                 </div>
                 <div className='col-8'>
-                    Options
+                <select className='form-select' aria-label='Default select' disabled={disable}>
+                    <option defaultValue={true} className='small'>Select</option>
+                    {(filter==='Body Part')? optionsList(bodyParts) : ((filter==='Muscle Group')? optionsList(muscles) : optionsList(exerciseNames))}
+                   </select>
                 </div>
             </div>
         </div>
