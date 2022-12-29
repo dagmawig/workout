3fbef54497be4068f7c61a521dcefe4020e9af9f
dagmawig <dgebreselasse@gmail.com>
2022-12-28 22:08:59 -0600

    added styling to search and header components

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './Search.css';
 var muscles = require('../exerMuscle.json');
 var exerciseNames = require('../exerciseNames.json');
@@ -6,38 +6,48 @@ var bodyParts = require('../exerBody.json');
 var exercisesLocal = require('../exercisesLocal.json');
 
 function Search() {
-
+    console.log(exercisesLocal[991])
     const [filter, updateFilter] = useState(null);
     const [disable, updateDisable] = useState(true);
     const [exerFilter, updateExer] = useState([]);
     const [hidden, updateHidden] = useState(true);
+    const[myRef] = useState(React.createRef());
 
     function optionsList(list) {
         return list.map((item, i) => {
             return (
-                <option value={item} className='small' key ={i+'option'}>{item}</option>
+                <option value={item} className='small' key={i + 'option'}>{item}</option>
             )
         })
     }
 
     function exercisesList(list) {
-        return list.map((item, i) => { 
+        return list.map((item, i) => {
             return (
-                <div className='row exercise' key={i+'exer'}>
-                    <div className='col-6 search_text'>
+                <div className='row exercise' key={i + 'exer'}>
+                    <div className='col-7 search_text'>
                         <div className='row text'>
-                            <div className='col-6 name'>Workout Name</div>
-                            <div className='col-6 name_value'>{item.name}</div>
-                            <div className='col-6 body'>Body Part</div>
-                            <div className='col-6 body_value'>{item.bodyPart}</div>
-                            <div className='col-6 equipment'>Equipment</div>
-                            <div className='col-6 equip_value'>{item.equipment}</div>
-                            <div className='col-6 target'>Target</div>
-                            <div className='col-6 target_value'>{item.target}</div>
+                            <div className='row first'>
+                            <div className='col-12 name'><b>Workout</b></div>
+                            <div className='col-12 name_value'>{item.name}</div>
+                            </div>
+                            <div className='row second'>
+                            <div className='col-12 body'><b>Body Part</b></div>
+                            <div className='col-12 body_value'>{item.bodyPart}</div>
+                            </div>
+                            <div className='row third'>
+                            <div className='col-12 equipment'><b>Equipment</b></div>
+                            <div className='col-12 equip_value'>{item.equipment}</div>
+                            </div>
+                            <div className='row fourth'>
+                            <div className='col-12 target'><b>Target</b></div>
+                            <div className='col-12 target_value'>{item.target}</div>
+                            </div>
+                            
                         </div>
                     </div>
-                    <div className='col-6 search_gif'>
-                    <img src={'/images/'+item.localUrl} alt="my-gif" />
+                    <div className='col-5 search_gif'>
+                        <img src={'/images/' + item.localUrl} style={{backgroundColor: 'white'}} alt="my-gif" />
                     </div>
                 </div>
             )
@@ -48,7 +58,7 @@ function Search() {
         updateFilter(value);
         document.getElementById('option').selectedIndex = 0;
         updateExer([])
-        if(value==='empty') {
+        if (value === 'empty') {
             updateDisable(true);
             updateHidden(true);
         }
@@ -59,9 +69,11 @@ function Search() {
     }
 
     function handleChangeOpt(value) {
-        if(filter==='Body Part') updateExer(exercisesLocal.filter(exer=>exer.bodyPart===value));
-        else if(filter==='Muscle Group') updateExer(exercisesLocal.filter(exer=>exer.target===value))
-        else if(filter==='Workout Name') updateExer(exercisesLocal.filter(exer=>exer.name===value))
+        myRef.current.scrollTo(0, 0);
+        
+        if (filter === 'Body Part') updateExer(exercisesLocal.filter(exer => exer.bodyPart === value));
+        else if (filter === 'Muscle Group') updateExer(exercisesLocal.filter(exer => exer.target === value))
+        else if (filter === 'Workout Name') updateExer(exercisesLocal.filter(exer => exer.name === value))
         else updateExer([]);
     }
 
@@ -69,32 +81,32 @@ function Search() {
         <div className="search container">
             <div className='row search_select'>
                 <div className='row filter'>
-                <div className='col-4'>
-                    Search By :
-                </div>
-                <div className='col-8'>
-                   <select className='form-select' aria-label='Default select' id='filter' onChange={e=>handleChange(e.target.value)}>
-                    <option defaultValue={true} className='small' value='empty'>Select</option>
-                    <option value='Body Part' className='small'>Body Part</option>
-                    <option value='Muscle Group' className='small'>Muscle Group</option>
-                    <option value='Workout Name' className='small'>Workout Name</option>
-                   </select>
-                </div>
+                    <div className='col-4'>
+                        <b>Search By:</b>
+                    </div>
+                    <div className='col-8'>
+                        <select className='form-select' aria-label='Default select' id='filter' onChange={e => handleChange(e.target.value)}>
+                            <option defaultValue={true} className='small' value='empty'>Select</option>
+                            <option value='Body Part' className='small'>Body Part</option>
+                            <option value='Muscle Group' className='small'>Muscle Group</option>
+                            <option value='Workout Name' className='small'>Workout Name</option>
+                        </select>
+                    </div>
                 </div>
                 <div className='row options' hidden={hidden}>
-                <div className='col-4'>
-                    Options :
+                    <div className='col-4'>
+                        <b>Options:</b>
+                    </div>
+                    <div className='col-8'>
+                        <select className='form-select' id='option' aria-label='Default select' disabled={disable} onChange={e => handleChangeOpt(e.target.value)}>
+                            <option defaultValue={true} className='small' value='empty'>Select</option>
+                            {(filter === 'Body Part') ? optionsList(bodyParts) : ((filter === 'Muscle Group') ? optionsList(muscles) : optionsList(exerciseNames))}
+                        </select>
+                    </div>
                 </div>
-                <div className='col-8'>
-                <select className='form-select' id='option' aria-label='Default select' disabled={disable} onChange={e=>handleChangeOpt(e.target.value)}>
-                    <option defaultValue={true} className='small' value='empty'>Select</option>
-                    {(filter==='Body Part')? optionsList(bodyParts) : ((filter==='Muscle Group')? optionsList(muscles) : optionsList(exerciseNames))}
-                   </select>
-                </div>
-                </div>
-                
+
             </div>
-            <div className='row search_exercises'>
+            <div ref={myRef} className='row search_exercises'>
                 {exercisesList(exerFilter)}
             </div>
         </div>
