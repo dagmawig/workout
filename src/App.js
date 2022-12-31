7ae61c3dee3ad19cabe151e1d5b206b020d7b356
dagmawig <dgebreselasse@gmail.com>
2022-12-30 18:40:40 -0600

    added footer element

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <div className="App">
       <Header/>
       <Search/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className='footer container bg-success'>
+            <div className='footer_row row'>
+                <div className='footer_search col-6'>
+                    <button className="footer_button"><i className="fa fa-search fa-2x"></i></button>
+                </div>
+                <div className='footer_profile col-6'>
+                    <button className="footer_button"><i className="fa fa-user fa-2x"></i></button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
