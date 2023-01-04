18c207b9e44dcaeaeb09fa4769455228cf32e63e
dagmawig <dgebreselasse@gmail.com>
2023-01-03 21:51:36 -0600

    activated footer button links, added react-router to DOM elements in app.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import './App.css';
 import Header from './components/Header';
 import Search from './components/Search';
 import Footer from './components/Footer';
-import Workout from './components/Workout'; 
+import Workout from './components/Workout';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Workout/>
-      <Footer/>
-    </div>
+    <Router >
+      <div className="App">
+        <Routes>
+          <Route path="/" element={
+          <>
+          <Header/>
+          <Search/>
+          <Footer/>
+          </>
+          }/>
+          <Route path="/workout" element={
+          <>
+          <Header/>
+          <Workout/>
+          <Footer/>
+          </>
+        }/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './Footer.css';
+import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
         <div className='footer container bg-success'>
             <div className='footer_row row'>
                 <div className='footer_search col-6'>
-                    <button className="footer_button"><i className="fa fa-dumbbell fa-2x"></i></button>
+                    <Link to="/">
+                        <button className="footer_button"><i className="fa fa-dumbbell fa-2x"></i></button>
+                    </Link>
                 </div>
                 <div className='footer_profile col-6'>
-                    <button className="footer_button"><i className="fa fa-plus fa-2x"></i></button>
+                    <Link to="/workout">
+                        <button className="footer_button"><i className="fa fa-plus fa-2x"></i></button>
+                    </Link>
                 </div>
             </div>
         </div>
